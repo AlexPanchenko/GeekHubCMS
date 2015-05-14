a6c9368a348980303e18b6f74925590b6a8f1d78
Aleksandr_Kosholap <kosholap@gmail.com>
2015-05-14 21:38:27 +0300

    test add entity Question, Answer

---
+++ core/src/main/java/org/geekhub/entity/Answer.java
@@ -0,0 +1,64 @@
+package org.geekhub.entity;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+
+/**
+ * Created by helldes on 14.05.2015.
+ */
+@Entity
+@Table(name = "ANSWER")
+public class Answer {
+    @Id
+    @GeneratedValue
+    @Column(name = "ANSWER_ID", unique = true, nullable = false)
+    private int id;
+
+    @Column(name = "ANSWER_TEXT", unique = true, nullable = false)
+    @NotBlank(message = "Answer text should be not empty")
+    private String answerText;
+
+    @Column(name = "ANSWER_RIGHT", nullable = false)
+    @NotBlank(message = "Right answer should be not empty")
+    private Boolean answerRight;
+
+    @ManyToOne
+    @JoinColumn(name = "QUESTION_ID")
+    private Question question;
+
+    public Answer(){
+    }
+
+    public Boolean getAnswerRight() {
+        return answerRight;
+    }
+
+    public void setAnswerRight(Boolean answerRight) {
+        this.answerRight = answerRight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}
---
+++ core/src/main/java/org/geekhub/entity/Question.java
@@ -0,0 +1,77 @@
+package org.geekhub.entity;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by helldes on 14.05.2015.
+ */
+
+@Entity
+@Table(name = "QUESTION")
+public class Question {
+    @Id
+    @GeneratedValue
+    @Column(name = "QUESTION_ID", unique = true, nullable = false)
+    private int id;
+
+    @Column(name = "QUESTION_TEXT", unique = true, nullable = false)
+    @NotBlank(message = "Question text should be not empty")
+    private String questionText;
+
+    @Column(name = "QUESTION_WEIGTH", nullable = false)
+    @NotBlank(message = "Question weigth should be not empty")
+    private Byte questionWeigth;
+
+    @Column(name = "COURSE_ID", nullable = false)
+    @NotBlank(message = "COURSE ID should be not empty")
+    private int courseId;
+
+    @OneToMany(fetch = FetchType.LAZY,mappedBy="answer")
+    private Set<Answer> answers;
+
+    public Question() {
+    }
+
+    public Byte getQuestionWeigth() {
+        return questionWeigth;
+    }
+
+    public void setQuestionWeigth(Byte questionWeigth) {
+        this.questionWeigth = questionWeigth;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public Set<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Set<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+}
