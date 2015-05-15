761098cdac877f0497fd7582161cc2f57c6bff2f
Denis Kliuch <denysko.kl@gmail.com>
2015-05-15 16:43:58 +0300

    add jsp regis courses

---
+++ app/src/main/java/org/geekhub/controllers/RegisCoursesController.java
@@ -0,0 +1,17 @@
+package org.geekhub.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RegisCoursesController {
+
+    @RequestMapping(value = "/registrationCourses", method = RequestMethod.POST)
+    public String registrationCourses( ) {
+
+
+        return "registrationCourses";
+    }
+}
