42354b79ab346a8be684b3f24ff58b4370632c41
odahovskiy <odahovskiy@gmail.com>
2015-05-18 10:25:16 +0300

    Fill course bean by fields

--- core/src/main/java/org/geekhub/hibernate/bean/CourseBean.java
+++ core/src/main/java/org/geekhub/hibernate/bean/CourseBean.java
@@ -1,7 +1,59 @@
 package org.geekhub.hibernate.bean;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Aleksander on 15.05.2015.
+ *
+ * @Author Aleksander
+ * @Author odahovskiy
  */
 public class CourseBean {
+
+    private int id;
+
+    @NotBlank(message = "Name should be not empty")
+    private String name;
+
+    @NotBlank(message = "Description should be not empty")
+    private String description;
+
+    private Set<UserBean> users = new HashSet<>();
+
+    public CourseBean(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<UserBean> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<UserBean> users) {
+        this.users = users;
+    }
 }
