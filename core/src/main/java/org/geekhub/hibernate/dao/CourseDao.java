d458abb990d556107fb97e2fe9f6abcd5c028edd
odahovskiy <odahovskiy@gmail.com>
2015-05-18 10:30:08 +0300

    Added course get all method for pagination and implement it

--- core/src/main/java/org/geekhub/hibernate/dao/CourseDao.java
+++ core/src/main/java/org/geekhub/hibernate/dao/CourseDao.java
@@ -2,8 +2,11 @@ package org.geekhub.hibernate.dao;
 
 import org.geekhub.hibernate.entity.Course;
 
+import java.util.List;
+
 /**
  * Created by helldes on 15.05.2015.
  */
 public interface CourseDao extends GenericDao<Course> {
+    List<Course> getAll(int page, int recordsPerPage);
 }
--- core/src/main/java/org/geekhub/hibernate/dao/impl/CourseDaoImpl.java
+++ core/src/main/java/org/geekhub/hibernate/dao/impl/CourseDaoImpl.java
@@ -3,8 +3,19 @@ package org.geekhub.hibernate.dao.impl;
 import org.geekhub.hibernate.dao.CourseDao;
 import org.geekhub.hibernate.entity.Course;
 
+import java.util.List;
+
 /**
  * Created by helldes on 15.05.2015.
  */
 public class CourseDaoImpl extends GenericDaoImpl<Course> implements CourseDao {
+    @Override
+    public List<Course> getAll(int page, int recordsPerPage) {
+        page = page * recordsPerPage - recordsPerPage;
+        return sessionFactory.getCurrentSession()
+                .createCriteria(Course.class)
+                .setFirstResult(page)
+                .setMaxResults(recordsPerPage)
+                .list();
+    }
 }
