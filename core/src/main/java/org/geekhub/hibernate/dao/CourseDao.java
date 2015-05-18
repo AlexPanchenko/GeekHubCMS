b2ed3915a6b4d4efc3faea03e5e9887d49ad4537
odahovskiy <odahovskiy@gmail.com>
2015-05-18 12:17:47 +0300

    Extend courses dao by update, delete and getAll methods  and implement it

--- core/src/main/java/org/geekhub/hibernate/dao/CourseDao.java
+++ core/src/main/java/org/geekhub/hibernate/dao/CourseDao.java
@@ -9,4 +9,6 @@ import java.util.List;
  */
 public interface CourseDao extends GenericDao<Course> {
     List<Course> getAll(int page, int recordsPerPage);
+    void updateCourse(Course course);
+    void deleteCourse(int courseId);
 }
