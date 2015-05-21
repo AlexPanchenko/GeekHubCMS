e81a4842af7f1f7caca37ef800f8b02a95e977a6
Aleksandr_Kosholap <kosholap@gmail.com>
2015-05-21 14:14:35 +0300

    create TestAssignment (DAO, Service, Bean)

---
+++ app/src/main/java/org/geekhub/controllers/TestController.java
@@ -0,0 +1,60 @@
+package org.geekhub.controllers;
+
+import org.geekhub.hibernate.bean.CourseBean;
+import org.geekhub.hibernate.bean.TestAssignmentBean;
+import org.geekhub.hibernate.bean.TestConfigBeen;
+import org.geekhub.service.CourseService;
+import org.geekhub.service.TestAssignmentService;
+import org.geekhub.service.TestConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * Created by helldes on 21.05.2015.
+ */
+@Controller
+@RequestMapping(value = "/student/test")
+public class TestController {
+    @Autowired
+    private CourseService courseService;
+
+    @Autowired
+    private TestAssignmentService testAssignmentService;
+
+    @Autowired
+    private TestConfigService testConfigService;
+
+    @RequestMapping(value = "/selectCourse", method = RequestMethod.GET)
+    public String selectCourse(ModelMap model) {
+        List<CourseBean> courseBeanList = courseService.getCourseBeenByUser();
+        model.addAttribute("coursesList", courseBeanList);
+        return "test-page/selectCourse";
+    }
+
+    @RequestMapping(value = "/selectCourse", method = RequestMethod.POST)
+    public String selectCourse(@RequestParam("courseId")String courseId) {
+        return "redirect:/student/test/course/" + courseId;
+    }
+
+    @RequestMapping(value = "/course/{courseId}/selectTest", method = RequestMethod.GET)
+    public String selectTest( @PathVariable("courseId") int courseId,
+                            ModelMap model) {
+        List<TestAssignmentBean> listTestAssignmentBean = testAssignmentService.getTAByUserAndCourse(courseId);
+        model.addAttribute("testList", testAssignmentService);
+        return "test-page/selectTest";
+    }
+
+    @RequestMapping(value = "/course/{courseId}/selectTest/{testId}", method = RequestMethod.POST)
+    public String selectTest(@RequestParam("courseId")String courseId,
+                             @RequestParam("testId")String testId) {
+        return "redirect:/student/test/course/" + courseId + "/test/" + testId;
+    }
+
+}
---
+++ core/src/main/java/org/geekhub/service/impl/TestAssignmentServiceImpl.java
@@ -0,0 +1,17 @@
+package org.geekhub.service.impl;
+
+import org.geekhub.hibernate.bean.TestAssignmentBean;
+import org.geekhub.service.TestAssignmentService;
+
+import java.util.List;
+
+/**
+ * Created by helldes on 21.05.2015.
+ */
+public class TestAssignmentServiceImpl implements TestAssignmentService {
+
+    @Override
+    public List<TestAssignmentBean> getTAByUserAndCourse(int courseId) {
+        return null;
+    }
+}
