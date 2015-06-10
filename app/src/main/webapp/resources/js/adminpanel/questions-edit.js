5d02505158578abd5eec5010c05019e3d4407063
vlad-musienko <asmodianko@gmail.com>
2015-06-10 15:42:34 +0300

    formation json, work in progress

--- app/src/main/webapp/resources/js/adminpanel/questions-edit.js
+++ app/src/main/webapp/resources/js/adminpanel/questions-edit.js
@@ -1,41 +1,57 @@
 var addNewAnswer = function () {
-    var inputGrp = $("<div></div>").addClass("input-group").appendTo($('.answer-box'));
+    var inputGrp = $("<div></div>").addClass("input-group").attr("id", "answer" + id).appendTo($('.answer-box'));
     var checkboxWrap = $("<span></span>").addClass("input-group-addon").attr("id", "basic-addon1").appendTo(inputGrp);
     $('<input />', {
-        type: 'checkbox'
+        type: 'checkbox',
+        "class": "right-answer-cb"
     }).addClass("").appendTo(checkboxWrap);
     $('<input />', {
         "type": 'text',
         "value": name,
         "aria-describedby": "basic-addon1",
-        "class": "form-control"
+        "class": "form-control answer-input"
     }).appendTo(inputGrp);
     var deleteWrap = $("<span></span>").addClass("input-group-addon").attr("id", "basic-addon2").appendTo(inputGrp);
     var deleteHref = $("<a/>").addClass("fa fa-times deleteAnswer").appendTo(deleteWrap);
 };
 
-var updateQuestion = function (){
-    //
+var updateQuestion = function () {
+    var answersArray = [];
+    var tmpArray = [];
+    var inputs = $(".input-group");
+    answersArray = [].map.call(inputs, function (el) {
+        var answer = {};
+        answer.id = "answer" + ($(el).attr("id") || 0);
+        answer.text = $(el).find(".answer-input").val();
+        answer.right = $(el).find(".right-answer-cb").prop('checked');
+        return answer;
+
+    });
+
+
+    console.log(answers);
+
 };
 
-$("#addAnswer").on("click", function(){
-   addNewAnswer();
+$("#addAnswer").on("click", function () {
+    addNewAnswer();
 });
 
-$(".answer-box").on("click", function(event){
+$(".answer-box").on("click", function (event) {
     target = event.target;
 
-    if($(target).hasClass("deleteAnswer")){
+    if ($(target).hasClass("deleteAnswer")) {
         event.preventDefault();
+        $(target).parent().parent().remove();
     }
 
-    if($(target).attr("type") == "checkbox"){
+    if ($(target).attr("type") == "checkbox") {
         $(target).parent().toggleClass("right-answer");
     }
 
 });
 
 
-$(function(){
-   addNewAnswer();
+$(function () {
+    addNewAnswer();
 });
