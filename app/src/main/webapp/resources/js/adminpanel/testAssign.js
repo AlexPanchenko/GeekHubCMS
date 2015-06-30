e488dbeaaf89a9bf5167497979824855e53b7d70
vlad-musienko <asmodianko@gmail.com>
2015-06-30 15:35:27 +0300

    test assign fix

--- app/src/main/webapp/resources/js/adminpanel/testAssign.js
+++ app/src/main/webapp/resources/js/adminpanel/testAssign.js
@@ -1,9 +1,17 @@
-$("#select-all").on("click", function (e) {
-    e.preventDefault();
-    $(':checkbox').each(function () {
-        if($(this).attr("disabled")){
-            return;
-        }
-        this.checked = !this.checked;
+(function ($) {
+    $("#select-all").on("click", function (e) {
+        e.preventDefault();
+        $(':checkbox').each(function () {
+            if ($(this).attr("disabled")) {
+                return;
+            }
+            this.checked = !this.checked;
+        });
     });
-});
+
+
+    $("#selectTestConfig").on("change", function () {
+        var configId = $(this).find(":selected").val();
+        window.location = config.url + "/admin/assignTest/" + configId;
+    });
+})(jQuery);
